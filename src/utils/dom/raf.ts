e5f66a9af55bc6e322087869aeb49838387483f9
vita2333 <lp_vitadolce@163.com>
2020-02-12 15:51:29 +0800

    feat: requestAnimationFrame

---
+++ src/utils/dom/raf.ts
@@ -0,0 +1,36 @@
+/**
+ * requestAnimationFrame polyfill
+ */
+
+import { isServer } from '..';
+
+let prev = Date.now();
+
+function fallback(fn: FrameRequestCallback): number {
+  const curr = Date.now();
+  const ms = Math.max(0, 16 - (curr - prev));
+  const id = setTimeout(fn, ms);
+  prev = curr + ms;
+  return id;
+}
+
+const root = <Window>(isServer ? global : window);
+
+const iRaf = root.requestAnimationFrame || fallback;
+
+const iCancel = root.cancelAnimationFrame || root.clearTimeout;
+
+export function raf(fn: FrameRequestCallback): number {
+  return iRaf.call(root, fn);
+}
+
+// double raf for animation
+export function doubleRaf(fn: FrameRequestCallback): void {
+  raf(() => {
+    raf(fn);
+  });
+}
+
+export function cancelRaf(id: number) {
+  iCancel.call(root, id);
+}
